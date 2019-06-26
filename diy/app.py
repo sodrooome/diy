ec89dcb2985c90ade3e0286b7255546b7323f72d
Ryan Febriansyah <15523163@students.uii.ac.id>
2019-06-26 11:24:08 +0700

    parametrize routers

--- diy/app.py
+++ diy/app.py
@@ -1,3 +1,17 @@
-from api import API, RequestAPI
+from api import API, RequestAPI, UserRequest, UserRequestHandler
+app = UserRequestHandler()
 
-app = RequestAPI()
+@app.route("/home")
+def home(request, response):
+
+    response.text = "Hello from home page"
+
+@app.route("/about")
+def about(request, response):
+
+    response.text = "Hello from about page"
+
+@app.route("/post/{topic}")
+def topic(request, response, name):
+
+    response.text = "This post is {topic}"
