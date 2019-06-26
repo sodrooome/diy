27238d54d179063a512a7870875597f2a541cc6d
Ryan Febriansyah <15523163@students.uii.ac.id>
2019-06-26 15:01:23 +0700

    added vuepress for documentation

---
+++ .vuepress/config.js
@@ -0,0 +1,21 @@
+module.exports = {
+
+    title: 'Do It Yourself Framework',
+    themeConfig: {
+
+        nav: [
+
+            { text: 'Home', link: '/'},
+            { text: 'Index', link: '/index/'}
+
+        ],
+        sidebar: [
+
+            '/',
+            '/index/'
+
+        ]
+
+    }
+
+}
