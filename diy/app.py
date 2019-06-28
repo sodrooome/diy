f650135bf15690f9e74d64698f56caee3a83b7f2
Ryan Febriansyah <15523163@students.uii.ac.id>
2019-06-28 22:57:15 +0700

    new example for new route

--- diy/app.py
+++ diy/app.py
@@ -15,3 +15,23 @@ def about(request, response):
 def topic(request, response, topic):
 
     response.text = f"This post is {topic}"
+
+@app.route("/new")
+class NewTopics:
+    def get(self, request, response):
+
+        response.text = "This is new topic"
+
+# example of new routing
+
+def handler(request, response):
+
+    response.text = "test new route"
+
+app.route_url("/test", handler)
+
+def eject(request, response):
+
+    response.text = "eject new test"
+
+app.route_url("/eject", eject)
