6014ea6555201010263d740effa56ce2b16638f2
Ryan Febriansyah <15523163@students.uii.ac.id>
2019-06-29 22:09:50 +0700

    second example for running

--- diy/serve.py
+++ diy/serve.py
@@ -4,6 +4,7 @@ Run in terminal with `python serve.py`
 """
 
 from waitress import serve
-import app
+import app, urls
 
 serve(app.app, host='127.0.0.1', port=8080)
+# serve(urls.urls, host='127.0.0.1', port=8000)
